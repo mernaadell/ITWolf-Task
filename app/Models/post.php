6e13c8907b2fe3a9663f915a02cc728d5f079a5d
mernaadell <mernaadel963@gmail.com>
2021-06-23 08:15:06 -0400

    create models and migrations

---
+++ app/Models/post.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class post extends Model
+{
+    use HasFactory;
+
+    protected $fillable=[
+        "title","content","image"
+    ];
+
+    public function category(){
+
+        return $this->belongsTo(category::class);
+    }
+
+    public function user(){
+
+        return $this->belongsTo(user::class);
+    }
+}
